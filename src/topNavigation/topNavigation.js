6505974dc3fdef1675fe47dadc07b6600e928937
Ting <badonlineqq@gmail.com>
2020-08-22 13:56:35 +1000

    background color -> grey change icons & names

--- src/topNavigation/topNavigation.js
+++ src/topNavigation/topNavigation.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import {
+  makeStyles
+}
+from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 import AppBar from '@material-ui/core/AppBar';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -9,8 +12,8 @@ import Typography from '@material-ui/core/Typography';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
+import AccessAlarmIcon from '@material-ui/icons/AccessAlarm';
+import HomeIcon from '@material-ui/icons/Home';
 import Main from '../demoMain/demoMain.js'
 
 const drawerWidth = 240;
@@ -21,6 +24,7 @@ const useStyles = makeStyles((theme) => ({
   },
   appBar: {
     zIndex: theme.zIndex.drawer + 1,
+    background: 'grey'
   },
   drawer: {
     width: drawerWidth,
@@ -37,38 +41,45 @@ const useStyles = makeStyles((theme) => ({
 export default function ClippedDrawer() {
   const classes = useStyles();
 
-  return (
-    <div className={classes.root}>
-      <CssBaseline />
-      <AppBar position="fixed" className={classes.appBar}>
-        <Toolbar>
-          <Typography variant="h6" noWrap>
-            BRAND NAME
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Drawer
-        className={classes.drawer}
-        variant="permanent"
-        classes={{
-          paper: classes.drawerPaper,
-        }}
-      >
-        <Toolbar />
-        <div className={classes.drawerContainer}>
-          <List>
-            {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-              <ListItem button key={text}>
-                <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItem>
-            ))}
-          </List>
-        </div>
-      </Drawer>
-      <main className={classes.content}>
-        <Main/>
-      </main>
-    </div>
-  );
+  return ( < div className = {
+      classes.root
+    } >
+    < CssBaseline / >
+    < AppBar position = "fixed"
+    className = {
+      classes.appBar
+    } >
+    < Toolbar >
+    < Typography variant = "h6"
+    noWrap >
+    BRAND NAME < /Typography> < /Toolbar > < /AppBar> < Drawer className = {
+    classes.drawer
+  }
+  variant = "permanent"
+  classes = {
+      {
+        paper: classes.drawerPaper,
+      }
+    } >
+    < Toolbar / >
+    < div className = {
+      classes.drawerContainer
+    } >
+    < List > {
+      ['Home', 'News'].map((text, index) => ( < ListItem button key = {
+            text
+          } >
+          < ListItemIcon > {
+            index % 2 === 0 ? < HomeIcon / > : < AccessAlarmIcon / >
+          } < /ListItemIcon> < ListItemText primary = {
+          text
+        }
+        /> < /ListItem >
+      ))
+} < /List> < /div > < /Drawer> < main className = {
+classes.content
+} >
+< Main / >
+  < /main> < /div >
+);
 }
